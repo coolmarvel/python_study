0800d53c690a948b883a948020b41bab9724368c
coolmarvel <marvel97@naver.com>
2022-05-31 11:00:02 +0900

    string study

---
+++ string.py
@@ -0,0 +1,23 @@
+# 문자열 공부
+
+# %s : 문자열
+# %d : 정수(int)
+# %f : 소수(float)
+# %c : 문자(char)
+
+say = "Python is very easy."
+a = "Life is too short. You need Python"
+b = "Python"
+c = " is fun!"
+d = 2
+e = "Pithon"
+f = "y"
+g = "I eat %d apples." % 3
+number = 3
+h = "I eat %d bananas." % number
+aa = "Error is %d%%" % 98
+bb = "I eat {0} apples".format("five")
+cc = "I ate {0} apples. so I was sick {1} days".format("three", 2)
+dd = "I ate {number} apples. so I was sick {days} days".format(number="three", days=2)
+print(cc)
+print("="*50)
